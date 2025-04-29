9cf94e54c2bdb67226c8d746cb20bf03d48b0a34
annechantpx <anne.chan@tpximpact.com>
2025-04-29 15:57:11 +0100

    moving ac prototype to main

--- app/routes.js
+++ app/routes.js
@@ -11,45 +11,143 @@ const util = require('util')
 
 router.post('/3-section', function(request, response) {
 
-    var typology = request.session.data['typology']
+    var sites = request.session.data['sites']
 
-    if (typology == "Urban dense"){
+    if (sites == "Yes"){
         response.redirect("/3a-section")
-    } else if (typology == "Rural and dispersed"){
+    } else if (sites == "No"){
         response.redirect("/3b-section")
-    } else if (typology == "Mixed-use or urban fringe"){
-        response.redirect("/3c-section")
-    } else if (typology == "Notable heritage or conservation designations"){
-        response.redirect("/3d-section")
-    } else if (typology == "Rapidly growing/expanding"){
-        response.redirect("/3e-section")
     } else {
         response.redirect("/2-section")
     }
 })
 
+router.post('/4-section', function(request, response) {
+
+    var sites = request.session.data['settlement']
+
+    if (sites == "Yes"){
+        response.redirect("/6-section")
+    } else if (sites == "No"){
+        response.redirect("/4-section")
+    } else {
+        response.redirect("/3-section")
+    }
+})
+
 router.post('/5-section', function(request, response) {
 
-    
-    let localConsiderations = request.session.data['localConsiderations']
+    var sites = request.session.data['1000']
 
-    localConsiderations = Array.isArray(localConsiderations) ? localConsiderations : []
-
-    if (localConsiderations.includes("Coastal areas") && (localConsiderations.includes("Proximity to an airport") && (localConsiderations.includes("Heavy tourism based")))){
-        response.redirect("/10a-section")
-    } else if (localConsiderations.includes("Proximity to an airport") && (localConsiderations.includes("Heavy tourism based"))){
-        response.redirect("/9a-section")
-    } else if (localConsiderations.includes("Coastal areas") && (localConsiderations.includes("Heavy tourism based"))){
-        response.redirect("/8a-section")
-    } else if (localConsiderations.includes("Coastal areas") && (localConsiderations.includes("Proximity to an airport"))){
-        response.redirect("/7a-section")
-    } else if (localConsiderations.includes("Coastal areas")){
-        response.redirect("/6a-section")
-    } else if (localConsiderations.includes("Proximity to an airport")){
-        response.redirect("/6b-section")
-    } else if (localConsiderations.includes("Heavy tourism based")){
-        response.redirect("/6c-section")
+    if (sites == "Yes"){
+        response.redirect("/6-section")
+    } else if (sites == "No"){
+        response.redirect("/5-section")
     } else {
         response.redirect("/4-section")
     }
 })
+
+router.post('/6-section', function(request, response) {
+
+    var sites = request.session.data['critical']
+
+    if (sites == "Yes"){
+        response.redirect("/6-section")
+    } else if (sites == "No"){
+        response.redirect("/6-section")
+    } else {
+        response.redirect("/5-section")
+    }
+})
+
+router.post('/7-section', function(request, response) {
+
+    var sites = request.session.data['critical']
+
+    if (sites == "Yes"){
+        response.redirect("/7-section")
+    } else if (sites == "No"){
+        response.redirect("/7-section")
+    } else {
+        response.redirect("/6-section")
+    }
+})
+
+router.post('/8-section', function(request, response) {
+
+    var sites = request.session.data['london']
+
+    if (sites == "Yes"){
+        response.redirect("/8-section")
+    } else if (sites == "No"){
+        response.redirect("/8-section")
+    } else {
+        response.redirect("/7-section")
+    }
+})
+
+router.post('/9-section', function(request, response) {
+
+    var sites = request.session.data['greenbelt']
+
+    if (sites == "Yes"){
+        response.redirect("/9-section")
+    } else if (sites == "No"){
+        response.redirect("/10-section")
+    } else {
+        response.redirect("/8-section")
+    }
+})
+
+router.post('/10-section', function(request, response) {
+
+    var sites = request.session.data['alter']
+
+    if (sites == "Yes"){
+        response.redirect("/review")
+    } else if (sites == "No"){
+        response.redirect("/10-section")
+    } else {
+        response.redirect("/9-section")
+    }
+})
+
+router.post('/11-section', function(request, response) {
+
+    var sites = request.session.data['alter']
+
+    if (sites == "Yes"){
+        response.redirect("/review")
+    } else if (sites == "No"){
+        response.redirect("/review")
+    } else {
+        response.redirect("/review")
+    }
+})
+
+//router.post('/5-section', function(request, response) {
+
+    
+  //  let localConsiderations = request.session.data['localConsiderations']
+
+  //  localConsiderations = Array.isArray(localConsiderations) ? localConsiderations : []
+
+ //   if (localConsiderations.includes("Coastal areas") && (localConsiderations.includes("Proximity to an airport") && (localConsiderations.includes("Heavy tourism based")))){
+  //      response.redirect("/10a-section")
+  //  } else if (localConsiderations.includes("Proximity to an airport") && (localConsiderations.includes("Heavy tourism based"))){
+  //      response.redirect("/9a-section")
+ //   } else if (localConsiderations.includes("Coastal areas") && (localConsiderations.includes("Heavy tourism based"))){
+  //      response.redirect("/8a-section")
+    //} else if (localConsiderations.includes("Coastal areas") && (localConsiderations.includes("Proximity to an airport"))){
+      //  response.redirect("/7a-section")
+    //} else if (localConsiderations.includes("Coastal areas")){
+    //    response.redirect("/6a-section")
+    //} else if (localConsiderations.includes("Proximity to an airport")){
+    //    response.redirect("/6b-section")
+    //} else if (localConsiderations.includes("Heavy tourism based")){
+      //  response.redirect("/6c-section")
+    //} else {
+      //  response.redirect("/4-section")
+    //}
+//})
